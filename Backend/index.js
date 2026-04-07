5be2fb827d990d5b78071172487b453081527f52
webs-surfer <itsrohanadhikari@gmail.com>
2026-04-07 12:23:08 +0530

    first route'

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,17 @@
 const express = require('express');
+const app = express();
+const port = 3000
+const data = [
+    {'id':1,'name':'rohan','age':10},
+    {'id':2,'name':'rohan','age':10},
+    {'id':3,'name':'rohan','age':10}
+]
+app.get('/',(req,res)=>{
+    res.json("server is running")
+})
+app.get('/data',(req,res)=>{
+    res.json(data)
+})
+app.listen(port,()=>{
+    console.log(`Server is running at ${port}`)
+})
